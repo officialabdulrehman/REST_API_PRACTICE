e5efac5427b0a69d7add56f76ce710adf00464b2
officialabdulrehman <arehman08@hotmail.com>
2020-11-19 12:29:17 +0500

    Connected database, added post model, updated controller and app.js

---
+++ server/controllers/feed.js
@@ -0,0 +1,41 @@
+const { validationResult } = require('express-validator/check')
+
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post',
+        imageUrl: 'images/Stream.jpg',
+        creator: {
+          name: 'Niz'
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
+}
+
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(422).json({
+      message: 'Validation failed',
+      errors: errors.array()
+    })
+  }
+  const { title, content } = req.body
+  res.status(201).json({
+    message: 'Post created successfully',
+    post:{
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: 'Niz'
+      },
+      createdAt: new Date()
+    }
+  })
+}
