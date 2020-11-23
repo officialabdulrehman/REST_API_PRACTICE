e3d271d1e5cb10b69c98a3936a02f0ddfa0213c5
officialabdulrehman <arehman08@hotmail.com>
2020-11-23 11:44:00 +0500

    Added updatePost controller, and a few changes in client

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const { validationResult } = require('express-validator/check')
 
 const Post = require('../models/post');
@@ -77,4 +80,60 @@ exports.getPost = (req, res, next) => {
       }
       next(err)
     })
+}
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    const error = new Error('Validation failed')
+    error.statusCode = 422
+    throw error
+  }
+
+  const postId = req.params.postId;
+  const {title, content} = req.body;
+  let imageUrl = req.body.image;
+
+  if(req.file){
+    imageUrl = req.file.path.replace("\\" ,"/");
+  }
+  if(!imageUrl){
+    const error = new Error('No image selected')
+    error.statusCode = 422;
+    throw error
+  }
+
+  Post.findById(postId)
+    .then(post => {
+      if(!post){
+        const error = new Error('Validation failed')
+        error.statusCode = 422
+        throw error
+      }
+      if(imageUrl !== post.imageUrl){
+        clearImage(post.imageUrl)
+      }
+      post.title = title
+      post.content = content
+      post.imageUrl = imageUrl
+      return post.save()
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Post updated',
+        post: result
+      })
+    })
+    .catch(err => {
+      if(!err.statusCode){
+        err.statusCode = 500
+      }
+      next(err)
+    })
+}
+
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath)
+  fs.unlink(filePath, err => console.log(err))
 }
