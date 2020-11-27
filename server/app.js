cb997c69e92d9ec7c0554ff49505353683c09af8
officialabdulrehman <arehman08@hotmail.com>
2020-11-27 19:01:11 +0500

    Fixed CORS error and established connection between server and client

--- server/app.js
+++ server/app.js
@@ -57,7 +57,12 @@ app.use((err, req, res, next) => {
 mongoose.connect(sensitive.MONGODB_URI)
   .then(result => {
     const server = app.listen(8080)
-    const io = require('socket.io')(server)
+    const io = require('socket.io')(server, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT"]
+      }
+    })
     io.on('connection', socket => {
       console.log('CLient connected ')
     })
