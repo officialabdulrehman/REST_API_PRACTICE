f7ba60559b9c5226f8eba9a2a5218699c280e6bc
officialabdulrehman <arehman08@hotmail.com>
2020-11-28 13:22:59 +0500

    added socket logic for deleting

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -13,7 +13,7 @@ exports.getPosts = async(req, res, next) => {
   const perPage = 2
   try {
     const totalItems = await Post.find().countDocuments()
-    const posts = await Post.find().populate('creator').skip((currentPage - 1) * perPage).limit(perPage)
+    const posts = await Post.find().populate('creator').sort({createdAt: -1}).skip((currentPage - 1) * perPage).limit(perPage)
 
     res.status(200).json({
       message: 'Posts fetched!!',
@@ -180,6 +180,7 @@ exports.deletePost = (req, res, next) => {
     })
     .then(result => {
       console.log(result)
+      io.getIO().emit('posts', {action: 'delete', post: postId})
       res.status(200).json({
         message: 'Post Deleted'
       })
